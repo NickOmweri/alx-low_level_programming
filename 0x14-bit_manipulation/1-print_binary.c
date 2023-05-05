fbebb278a93f0c63bb973a11c97249ec0bf96f3b
NickOmweri <nick.omweri@gmail.com>
2023-05-04 17:41:00 -0700

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,21 +7,21 @@
 
 void print_binary(unsigned long int n)
 {
-	int i, count = 0;
-	unsigned long int current;
+	int i, x = 0;
+	unsigned long int present;
 
 	for (i = 63; i >= 0; i--)
 	{
-		current = n >> i;
+		present = n >> i;
 
-		if (current & 1)
+		if (present & 1)
 		{
 			_putchar('1');
-			count++;
+			x++;
 		}
-		else if (count)
+		else if (x)
 			_putchar('0');
 	}
-	if (!count)
+	if (!x)
 		_putchar('0');
 }
